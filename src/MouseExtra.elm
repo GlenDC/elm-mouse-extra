9d03f5fc0636886d744e0893d734d0521c1805fd
glendc <decauwsemaecker.glen@gmail.com>
2015-06-28 15:05:39 +0000

    supporting a signal to check what mouseButtons are down

--- src/MouseExtra.elm
+++ src/MouseExtra.elm
@@ -1,7 +1,67 @@
-module MouseExtra where
+module MouseExtra 
+  ( ButtonCode
+  , buttonsDown
+  )
+  where
 
+import Set
 import Native.MouseExtra
 
 
-greeting : String -> String
-greeting = Native.MouseExtra.greeting
+{-| Type alias to make it clearer what integers are supposed to represent in this library. -}
+type alias ButtonCode = Int
+
+-- MANAGE RAW STREAMS
+
+type alias Model =
+    { buttonCodes : Set.Set ButtonCode
+    }
+
+
+empty : Model
+empty =
+    { buttonCodes = Set.empty
+    }
+
+
+type Event = Up EventInfo | Down EventInfo
+
+type alias EventInfo =
+    { buttonCode : ButtonCode
+    }
+
+
+update : Event -> Model -> Model
+update event model =
+  case event of
+    Down info ->
+        { buttonCodes = Set.insert info.buttonCode model.buttonCodes
+        }
+
+    Up info ->
+        { buttonCodes = Set.remove info.buttonCode model.buttonCodes
+        }
+
+
+model : Signal Model
+model =
+  Signal.foldp update empty rawEvents
+
+
+rawEvents : Signal Event
+rawEvents =
+  Signal.mergeMany
+    [ Signal.map Up Native.MouseExtra.ups
+    , Signal.map Down Native.MouseExtra.downs
+    ]
+
+
+dropMap : (a -> b) -> Signal a -> Signal b
+dropMap f signal =
+  Signal.dropRepeats (Signal.map f signal)
+
+
+{-| Set of keys that are currently down. -}
+buttonsDown : Signal (Set.Set ButtonCode)
+buttonsDown =
+  dropMap .buttonCodes model
--- src/Native/MouseExtra.js
+++ src/Native/MouseExtra.js
@@ -7,7 +7,42 @@ Elm.Native.MouseExtra.make = function(localRuntime) {
     return localRuntime.Native.MouseExtra.values;
   }
 
+  var NS = Elm.Native.Signal.make(localRuntime);
+  var Utils = Elm.Native.Utils.make(localRuntime);
+
+  var node = localRuntime.isFullscreen()
+    ? document
+    : localRuntime.node;
+
+  function mouseKeyEvent(event) {
+    return {
+      _: {},
+      buttonCode: event.button,
+      };
+  }
+
+  function mouseKeyStream(eventName, handler) {
+    var stream = NS.input(eventName, '\0');
+
+    localRuntime.addListener([stream.id], node, eventName, function(e) {
+      localRuntime.notify(stream.id, handler(e));
+    });
+
+    return stream;
+  }
+
+  // Disabling Context Menu (as it interferes with right click)
+  var contextMenu = NS.input('contextmenu', false);
+  localRuntime.addListener([contextMenu.id], node, 'contextmenu', function(e) {
+    e.preventDefault();
+  });
+
+  var downs = mouseKeyStream('mousedown', mouseKeyEvent);
+  var ups = mouseKeyStream('mouseup', mouseKeyEvent)
+
   return localRuntime.Native.MouseExtra.values = {
-    greeting : function(name) { console.log("Hello, " + name + "!"); return name; },
+    downs: downs,
+    ups: ups,
+    contextMenu: contextMenu,
   };
 };
